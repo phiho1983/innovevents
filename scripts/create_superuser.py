48fcffbbf995b0598586375dd8db10068c2e5b6b
phiho1983 <nguyenphio@gmail.com>
2026-02-18 22:24:45 +0100

    script creation superuser

---
+++ scripts/create_superuser.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@innovevents.local")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "1234")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"✅ Superuser créé : {username}")
+else:
+    print(f"ℹ️ Superuser déjà présent : {username}")
